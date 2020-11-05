6ae869b9960ac1e21ae0139f043d5b0bf0c16df4
Jena Holtzclaw <65194896+jholtzclaw@users.noreply.github.com>
2020-11-05 12:08:51 -0600

    mvp

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -22,7 +22,6 @@ const hide = (elem) => {
   elem.style.display = 'none';
 };
 
-// activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
 const getNotes = () =>
@@ -75,9 +74,9 @@ const handleNoteSave = () => {
   });
 };
 
-// Delete the clicked note
+
 const handleNoteDelete = (e) => {
-  // prevents the click listener for the list from being called when the button inside of it is clicked
+ 
   e.stopPropagation();
 
   const note = e.target;
@@ -93,14 +92,13 @@ const handleNoteDelete = (e) => {
   });
 };
 
-// Sets the activeNote and displays it
+
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
 };
 
-// Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
@@ -114,7 +112,7 @@ const handleRenderSaveBtn = () => {
   }
 };
 
-// Render the list of note titles
+
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
@@ -123,7 +121,7 @@ const renderNoteList = async (notes) => {
 
   let noteListItems = [];
 
-  // Returns HTML element with or without a delete button
+  
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
@@ -167,7 +165,7 @@ const renderNoteList = async (notes) => {
   }
 };
 
-// Gets notes from the db and renders them to the sidebar
+
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const router = require('express').Router()
-//needs update
 const { createNewNote } = require('../../libs/notes')
 const db = require('../../db/db')
-//needs update
 router.get('/notes', (req, res) => {
     let result = db
     if (result) {
@@ -14,14 +12,9 @@ router.get('/notes', (req, res) => {
 })
 router.post('/notes', (req, res) => {
     req.body.id = db.length.toString()
-    // if (!validateNote(req.body)) {
-    //     res.status(400).send('The note is not properly formatted.')
-    // } else {
-    // if (createNewNote) {
 
         const note = createNewNote(req.body, db)
 
-        res.json(note)
-    // }
+        res.json(note)   
 })
 module.exports = router
